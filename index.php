8a2d917adac88743185c052675e1670c8cc9ab06
RicardoEPS <reps3660@gmail.com>
2017-03-13 17:40:12 -0600

    Practica 10
    Laboratorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    //echo se utiliza para imprimir
+    /**/
+    #Comentarios
+    echo "Hola Mundo Php";
+?>
+<html>
+    <head>
+        <title>Prática 10 Php</title>
+    </head>
+    <body>
+        <h1>Hola Mundo Html</h1>
+    </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php
+    //echo se utiliza para imprimir
+    /**/
+    #Comentarios
+    echo "Hola Mundo Php";
+?>
+<html>
+    <head>
+        <title>Prática 10 Php</title>
+    </head>
+    <body>
+        <h1>Usando PHP</h1>
+        <p><?php
+            echo "Parrafo php";
+        ?></p>
+        <p>Parrafo Html</p>
+        <h3>
+            <?php
+            echo "Parrafo php";
+            ?>
+        </h3>
+    </body>
+</html>
